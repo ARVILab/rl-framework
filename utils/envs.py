d7d3c63bb5c3fcbeec964b5b7f0606b29decbf23
Ubuntu <poddiachyi@gmail.com>
2018-11-12 18:21:14 +0000

    init commit

---
+++ utils/envs.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+
+import torch
+
+import gym
+
+from baselines import bench
+from baselines.common.vec_env import VecEnvWrapper
+from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from baselines.common.vec_env.vec_normalize import VecNormalize as VecNormalize_
+
+
+def make_env(env_name, seed, process_id, log_dir):
+    def thunk():
+        env = gym.make(env_name)
+        env.seed(seed + process_id)
+        obs_shape = env.observation_space.shape
+        if log_dir is not None:
+            env = bench.Monitor(env, os.path.join(log_dir, str(process_id)),
+                                          allow_early_resets=False)
+        return env
+    return thunk
+
+def make_env_vec(env_name, seed, n_processes, gamma, log_dir, device):
+    envs = [make_env(env_name, seed, i, log_dir) for i in range(n_processes)]
+    if len(envs) > 1:
+        envs = SubprocVecEnv(envs)
+    else:
+        envs = DummyVecEnv(envs)
+    if len(envs.observation_space.shape) == 1:# for example len((4,)) == 1 for CartPole
+        if gamma is None:
+            envs = VecNormalize(envs, ret=False)
+        else:
+            envs = VecNormalize(envs, gamma=gamma)
+    envs = VecPyTorch(envs, device)
+    return envs
+
+class VecNormalize(VecNormalize_):
+    def __init__(self, *args, **kwargs):
+        super(VecNormalize, self).__init__(*args, **kwargs)
+        self.training = True
+
+    def _obfilt(self, obs):
+        if self.ob_rms:
+            if self.training:
+                self.ob_rms.update(obs)
+            obs = np.clip((obs - self.ob_rms.mean) / np.sqrt(self.ob_rms.var + self.epsilon),
+                                                                   -self.clipob, self.clipob)
+            return obs
+        else:
+            return obs
+
+    def train(self):
+        self.training = True
+
+    def eval(self):
+        self.training = False
+
+
+class VecPyTorch(VecEnvWrapper):
+    def __init__(self, venv, device):
+        super(VecPyTorch, self).__init__(venv)
+        self.device = device
+
+    def reset(self):
+        obs = self.venv.reset()
+        obs = torch.from_numpy(obs).float().to(self.device)
+        return obs
+
+    def step_async(self, actions):
+        actions = actions.squeeze(1).cpu().numpy()
+        self.venv.step_async(actions)
+
+    def step_wait(self):
+        obs, reward, done, info = self.venv.step_wait()
+        obs = torch.from_numpy(obs).float().to(self.device)
+        reward = torch.from_numpy(reward).unsqueeze(dim=1).float()
+        return obs, reward, done, info
