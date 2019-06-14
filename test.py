823e3292393ce00f4e4da9079e3a11bf8405092f
poddiachyi <poddiachyi@gmail.com>
2019-06-14 17:50:43 +0300

    clean up

--- test.py
+++ test.py
@@ -3,22 +3,15 @@ import torch
 import os
 from policies.policy import Policy
 from policies.random import RandomAgent
-from utils.envs import make_env_vec
 
 save_path = './saved_models'
 env_names = ['CartPole-v0', 'CartPole-v1', 'Acrobot-v1', 'MountainCar-v0', 'MountainCarContinuous-v0', 'Pendulum-v0']
-env_num = 2
+env_num = 0
 eps = 180
-
-# You will get am error ImportError: sys.meta_path is None, Python is likely shutting down
-# I tried to solved it but it basically the error on OpenAI side
-# It doesn't affect the environment running. It just a little annoying
-# Occurs when you use rendering
-
 
 def main():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    env = make_env_vec(env_names[env_num], 42, 1, None, None, device)
+    env = gym.make(env_names[0])
 
     policy = torch.load(os.path.join(save_path, '{}-{}.pt'.format(env_names[env_num], eps)))
     # policy = RandomAgent(env.action_space, device)
